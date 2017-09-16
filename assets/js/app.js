530a4a009c4f9dfd8ccc3a049cfaa54aafb2fcea
panforrest <guoqianp@gmail.com>
2017-09-16 13:27:42 -0400

    P3 21:16 mp3 uploaded under currentUser object s track

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@
 (function(){
 
   console.log('Hello Audio Stream')
+  var currentUser = null
 
   var turbo = Turbo({site_id: '59bc88062b047800127690e1'})
   turbo.currentUser(function(err, data){
@@ -11,7 +12,8 @@
   	  return
   	}
 
-    // console.log('Current User: ' + JSON.stringify(data))
+    console.log('Current User: ' + JSON.stringify(data))
+    currentUser = data.user
     $('#header-username').html(data.user.name)
 
   })
@@ -19,6 +21,38 @@
   $('#btn-upload').click(function(event){
   	event.preventDefault()
   	console.log('Upload Track')
+
+  	turbo.uploadFile(function(err, data){
+  	  if (err){
+  	  	alert('Error: ' + err.message)
+  	  	return
+  	  }
+
+  	  console.log('Upload Complete: ' + JSON.stringify(data))
+      //Upload Complete: {"confirmation":"success","result":{"site":"59bc88062b047800127690e1","name":"themepunch-script-1.mp3","type":"audio/mp3","url":"https://storage.turbo360.co/audio-stream--6xiqee/themepunch-script-1.mp3","size":184864,"timestamp":"2017-09-16T17:13:34.647Z","schema":"blob","id":"59bd5bbe2b047800127690f8"}}
+
+  	  var file = data.result
+
+  	  if (currentUser == null)
+  	  	return
+      
+      //UPDATE CURRENT USER
+      var tracks = currentUser.tracks || []
+      tracks.push(file)
+
+      turbo.update('user', currentUser, {tracks: tracks}, function(err, data){
+      	if(err){
+      	  alert('Error: ' + err.message)
+      	  return
+      	}
+
+      	console.log('USER UPDATED: ' + JSON.stringify(data))
+
+
+      })
+
+  	  
+  	})
   })
 
   $('#button-join').on('click', function(event){ //SHORTCUT $('#button-join').click(function(event){
