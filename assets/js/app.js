851d0fa2d9f6f7aa906aa90d71c676ff8cf6ffb9
panforrest <guoqianp@gmail.com>
2017-09-16 19:36:47 -0400

    14:30 uploaded icon is rendered right away

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,19 @@
       //Upload Complete: {"confirmation":"success","result":{"site":"59bc88062b047800127690e1","name":"themepunch-script-1.mp3","type":"audio/mp3","url":"https://storage.turbo360.co/audio-stream--6xiqee/themepunch-script-1.mp3","size":184864,"timestamp":"2017-09-16T17:13:34.647Z","schema":"blob","id":"59bd5bbe2b047800127690f8"}}
 
   	  if (uploading == 'icon'){
-  	  	console.log('ICON: ' + JSON.stringify(data))
+  	  	// console.log('ICON: ' + JSON.stringify(data))
+  	  	var image = data.result.url
+
+  	    turbo.update('user', currentUser, {image: image}, function(err, data){
+	      	if(err){
+	      	  alert('Error: ' + err.message)
+	      	  return
+	      	}
+
+	      	console.log('USER UPDATED: ' + JSON.stringify(data))
+	      	currentUser = data.result
+            $('#profile-icon').attr('src', currentUser.image)
+	    })
 
   	  	return
   	  }
@@ -81,6 +93,7 @@
     console.log('Tracks List: ' + tracksList)
 
     $('#tracks-table').html(tracksList)
+    $('#profile-icon').attr('src', currentUser.image)
 
   })
 
@@ -94,46 +107,6 @@
   	uploading = 'track'
   	uploadFile()
 
-  // 	turbo.uploadFile(function(err, data){
-  // 	  if (err){
-  // 	  	alert('Error: ' + err.message)
-  // 	  	return
-  // 	  }
-
-  // 	  console.log('Upload Complete: ' + JSON.stringify(data))
-  //     //Upload Complete: {"confirmation":"success","result":{"site":"59bc88062b047800127690e1","name":"themepunch-script-1.mp3","type":"audio/mp3","url":"https://storage.turbo360.co/audio-stream--6xiqee/themepunch-script-1.mp3","size":184864,"timestamp":"2017-09-16T17:13:34.647Z","schema":"blob","id":"59bd5bbe2b047800127690f8"}}
-
-  // 	  var file = data.result
-
-  // 	  if (currentUser == null)
-  // 	  	return
-      
-  //     //UPDATE CURRENT USER
-  //     var tracks = currentUser.tracks || []
-  //     tracks.push(file)
-
-  //     turbo.update('user', currentUser, {tracks: tracks}, function(err, data){
-  //     	if(err){
-  //     	  alert('Error: ' + err.message)
-  //     	  return
-  //     	}
-
-  //     	console.log('USER UPDATED: ' + JSON.stringify(data))
-  //     	currentUser = data.result
-
-	 //    var tracksList = ''
-	 //    currentUser.tracks.forEach(function(track, i){
-	 //      tracksList += '<tr><td style="width:130px"><a target="_blank" href="' + track.url + '" ><img src="/dist/images/icon_play.png" alt="..." /></a></td>'
-	 //      tracksList += '<td><h5><a target="_blank" href="' + track.url + '">' + track.name + '</a></h5><p>Uploaded: ' + track.timestamp + '</p></td>'
-	 //      tracksList += '<td><h4 class="price">Share</h4></td></tr>'
-
-	 //    })       	
-  //       $('#tracks-table').html(tracksList)
-
-  //     })
-
-  	  
-  // 	})
   })
 
   $('#button-join').on('click', function(event){ //SHORTCUT $('#button-join').click(function(event){
