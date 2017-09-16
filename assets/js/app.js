c7c6588d3e987afb8677d847362e19257f2c5661
panforrest <guoqianp@gmail.com>
2017-09-16 11:00:01 -0400

    end P2 user registration

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,9 +12,43 @@
   	password: ''
   }
 
-  $('#button-join').click(function(event){
+  $('#button-join').on('click', function(event){ //SHORTCUT $('#button-join').click(function(event){
   	event.preventDefault()
-  	console.log('Join button tapped')
+
+    var visitor = {
+  	  name: $('#input-name').val(),
+  	  email: $('#input-email').val(),
+      password: $('#input-password').val()
+  	}
+
+    if (visitor.name.length == 0){
+    	alert('Please enter your name')
+    	return
+    }
+
+    if (visitor.email.length == 0){
+    	alert('Please enter your email')
+    	return
+    }
+
+    if (visitor.password.length == 0){
+    	alert('Please enter your password')
+    	return
+    }
+
+
+
+  	
+  	turbo.createUser(visitor, function(err, data){
+  	  if (err){
+  	  	alert('Error: ' + err.message)
+  	  	return
+  	  }
+
+      console.log('Register: '+JSON.stringify(data))
+
+  	})
+
   })
 
 })()
